c5a6945d635f334df9b60659569ff602f9fdb065
root <root@s052d7d64.fastvps-server.com>
2023-03-24 16:51:47 +0300

    testing passed changed time config and encoding txt

--- test_linux.py
+++ test_linux.py
@@ -1,6 +1,6 @@
 def get_saved_files_names(source):
     try:
-        with open(f'uploaded_files_{source}.txt', 'r', encoding='cp1251') as uploaded_txt:
+        with open(f'uploaded_files_{source}.txt', 'r') as uploaded_txt:
             list_loaded_files = uploaded_txt.read()
         uploaded_txt.close()
         list_loaded_files = list_loaded_files.split('\n')[:-1]
@@ -8,7 +8,7 @@ def get_saved_files_names(source):
         list_loaded_files = []
     return list_loaded_files
 
-def write_saved_file_names(filename, source, encoding='utf8'):
+def write_saved_file_names(filename, source):
     with open(f'uploaded_files_{source}.txt', 'a+') as uploaded_txt:
         uploaded_txt.write(f"{filename}\n")
     uploaded_txt.close()
@@ -17,7 +17,7 @@ files_avito_current = get_saved_files_names('avito')
 
 print(files_avito_current)
 
-write_saved_file_names('23-03-23 (без дублей)', 'avito')
+write_saved_file_names('24-03-23 (без дублей)', 'avito')
 
 files_avito_current = get_saved_files_names('avito')
 
