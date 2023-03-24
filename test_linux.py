4fcbc235de4abd5a3359f7535c683771ca704dd4
Platon Kniazev <56154074+slowpokalipsis@users.noreply.github.com>
2023-03-24 22:49:27 +1000

    add test linux

---
+++ test_linux.py
@@ -0,0 +1,24 @@
+def get_saved_files_names(source):
+    try:
+        with open(f'uploaded_files_{source}.txt', 'r', encoding='cp1251') as uploaded_txt:
+            list_loaded_files = uploaded_txt.read()
+        uploaded_txt.close()
+        list_loaded_files = list_loaded_files.split('\n')[:-1]
+    except:
+        list_loaded_files = []
+    return list_loaded_files
+
+def write_saved_file_names(filename, source, encoding='utf8'):
+    with open(f'uploaded_files_{source}.txt', 'a+') as uploaded_txt:
+        uploaded_txt.write(f"{filename}\n")
+    uploaded_txt.close()
+
+files_avito_current = get_saved_files_names('avito')
+
+print(files_avito_current)
+
+write_saved_file_names('23-03-23 (без дублей)', 'avito')
+
+files_avito_current = get_saved_files_names('avito')
+
+print(files_avito_current)
