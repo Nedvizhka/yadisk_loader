0fc46ee6cb16cbc3544a9f5ffe36d05c53c67c75
Platon Kniazev <slowpokalipsis@gmail.com>
2023-04-21 13:00:17 +1000

    clear temp realty add 19 cian for testing on 20

--- main_utils.py
+++ main_utils.py
@@ -289,6 +289,17 @@ def update_realty(engine, df):
     clear_temp_table_query = \
         """DELETE FROM temp_realty"""
     try:
+        try:
+            # очистка данных из temp_realty
+            con_obj = engine.connect()
+            con_obj.execute(text(clear_temp_table_query))
+            con_obj.commit()
+            con_obj.close()
+            print('temp_realty очищена')
+            time.sleep(1)
+        except:
+            print('не удалось удалить данные')
+            time.sleep(1)
         # выгрузка данных в таблицу на сервере
         load_df_into_sql_table(df, 'temp_realty', engine)
 
